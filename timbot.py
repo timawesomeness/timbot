7d38d305df55d75ad02a885542eedfb4d1a88065
timawesomeness <git.pushes@timawesomeness.com>
2015-09-05 11:40:02 -0600

    Add logging option of what's sent License

--- timbot.py
+++ timbot.py
@@ -1,3 +1,18 @@
+#############################################################################
+# Copyright 2015 timawesomeness                                             #
+#                                                                           #
+# Licensed under the Apache License, Version 2.0 (the "License");           #
+# you may not use this file except in compliance with the License.          #
+# You may obtain a copy of the License at                                   #
+#                                                                           #
+#  http://www.apache.org/licenses/LICENSE-2.0                               #
+#                                                                           #
+# Unless required by applicable law or agreed to in writing, software       #
+# distributed under the License is distributed on an "AS IS" BASIS,         #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+# See the License for the specific language governing permissions and       #
+# limitations under the License.                                            #
+#############################################################################
 import socket, ssl, datetime, options, botcommands
 
 server = options.server
@@ -32,6 +47,7 @@ def partchan(chan):
 
 def sendmsg(chan, msg):
     print("Sending \"" + str(parsetxt(msg)).strip('b\'').strip('\'') + "\" to " + str(parsetxt(chan)).strip('b\'').strip('\''))
+    send("PRIVMSG " + "~#sent" + " :" + "Sending \"" + str(parsetxt(msg)).strip('b\'').strip('\'') + "\" to " + str(parsetxt(chan)).strip('b\'').strip('\'') + "\n")
     send("PRIVMSG " + chan + " :" + msg + "\n")
 
 # Connect, set nick, and join channels
